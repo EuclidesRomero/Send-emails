fdfec6b16b6662d259e9323416fdf961e8563ebc
Euclides Romero <euclidesantonioromeroibarra@gmail.com>
2024-07-19 16:56:54 -0500

    añadiendo archivo de configuracion del cliente

---
+++ config.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const awsConfig = {
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
+
+export {awsConfig}
